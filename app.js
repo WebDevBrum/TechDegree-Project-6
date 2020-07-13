5a6b8638dc011897c6c9c0c14483b8aaa2e79e05
Scott Davies <scottdavies1983@gmail.com>
2020-07-13 23:41:05 +0100

    site functionality in place

--- app.js
+++ app.js
@@ -11,17 +11,58 @@ app.use("/static", express.static("public"));
 
 app.get("/", (req, res) => {
   res.locals = projects;
-  res.render("index");
+  res.render("index", { projects });
+  console.log(projects[0].image_urls[0]);
 });
 
 app.get("/about", (req, res) => {
   res.render("about");
 });
 
-app.get("/project/:id", (req, res) => {
+app.get("/project/:id", (req, res, next) => {
   const { id } = req.params;
   res.locals = projects[id];
-  res.render("project");
+  //res.locals.project_name;
+  if (id < projects.length) {
+    res.render("project");
+  } else {
+    next();
+  }
+});
+
+app.use((req, res, next) => {
+  const err = new Error("Sorry, there's nothing to see here");
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+/*
+// development error handler
+// will print stacktrace
+if (app.get("env") === "development") {
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    console.log(err.message);
+    res.render("error", {
+      message: err.message,
+      error: err,
+    });
+  });
+}
+*/
+
+// production error handler
+// no stacktraces leaked to user
+app.use((err, req, res, next) => {
+  res.status(err.status);
+  console.log(err.status || 500);
+  res.render("error", {
+    message: err.message,
+    status: "Error:" + err.status,
+    error: {},
+  });
 });
 
 app.listen(3000, () => {
